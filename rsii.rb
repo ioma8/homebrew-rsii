7e3c53357edaff23cca24610b95d0a37abd82785
Jakub Kolčář <j.kolcar@raynet.cz>
2025-01-22 18:47:37 +0100

    Update RSII formula to support both Intel and ARM architectures with appropriate URLs and SHA256 checksums

--- rsii.rb
+++ rsii.rb
@@ -1,17 +1,20 @@
 class Rsii < Formula
   desc "RSII - Rust AI Command-line Assistant"
   homepage "https://github.com/ioma8/rsii"
-  url "https://github.com/ioma8/rsii/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "81caec15c60b09b49f95092f235058c91074ca2ccb9804c322bda2991134a35e"
+  if Hardware::CPU.intel?
+    url "https://github.com/ioma8/rsii/releases/download/0.1.1/rsii-macos-x86_64.tar.gz"
+    sha256 "1598358baa894fde9d849bbadf3f42da976a1eb8a02e736a5d099894e710b152"
+  else
+    url "https://github.com/ioma8/rsii/releases/download/0.1.1/rsii-macos-arm64.tar.gz"
+    sha256 "4f72c8783924170c5320aa37fd65ba934c119ba26d88aabccf824547d0e791b3"
+  end
   license "MIT"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "rsii"
   end
 
   test do
-    system "#{bin}/rsii", "--version"
+    assert_match "RSII version", shell_output("#{bin}/rsii --version")
   end
 end
